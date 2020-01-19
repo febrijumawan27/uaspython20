3006b0b69855cff53832d64f6a95e7bd9046c4eb
febrijumawan27 <57055098+febrijumawan27@users.noreply.github.com>
2020-01-19 12:12:29 +0700

    Add files via upload

---
+++ uaspy20-febri_jumawan/main.py
@@ -0,0 +1,25 @@
+from core.baseapp import BaseApp
+from data_model import book
+from data_model.book import Book
+
+class MainApp(BaseApp):
+    def _init_(self):
+        self.books = []
+        self.InputBook = []
+        self.ViewBook = []
+        BaseApp.__init__(self)
+
+class ViewBook(Book):
+    def _init_(self):
+        ViewBook = ViewBook (self,book)
+        ViewBook.list_book()
+
+if __name__ == "__main__":
+    app = MainApp()
+    app.run()
+
+    @property
+    def list_book(self):
+        return  self.list_book
+    def add_book(self):
+        return self.add_book()
